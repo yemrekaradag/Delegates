0d5ee66af6ef947932b6a62712746a6d9e9ecf0c
Yunus Emre Karadağ <yemrekaradag@hotmail.com>
2022-11-27 16:53:18 +0300

    Added 3 types of mean to program class.

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -1,3 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    delegate double Mean(double a, double b, double c);
 
+    static double Harmonic(double a, double b, double c)
+    {
+        return 3 / ((1 / a) + (1 / b) + (1 / c));
+    }
+
+    static void Main(string[] args)
+    {
+        Mean arithmetic = (a, b, c) => (a + b + c) / 3;
+        Mean geometric = delegate (double a, double b, double c)
+        {
+            return Math.Pow(a * b * c, 1 / 3.0);
+        };
+        Mean harmonic = Harmonic;
+        double arithmeticResult = arithmetic.Invoke(5, 6.5, 7);
+        double geometricResult = geometric.Invoke(5, 6.5, 7);
+        double harmonicResult = harmonic.Invoke(5, 6.5, 7);
+        Console.WriteLine($"Arithmetic Mean: {arithmeticResult} \nGeometric Mean: {geometricResult}\n" +
+            $"Harmonic Mean: {harmonicResult}");
+    }
+}
